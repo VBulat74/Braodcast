e09510fd75c2b455a277f19d084bab322b25ad62
VB <Vladimir.Bulat@gmail.com>
2024-02-29 13:12:16 +0300

    Lesson_01 (Create and register broadcast receiver)

--- app/src/main/java/ru/com/vbulat/braodcast/MainActivity.kt
+++ app/src/main/java/ru/com/vbulat/braodcast/MainActivity.kt
@@ -1,11 +1,24 @@
 package ru.com.vbulat.braodcast
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private val receiver = MyReceiver()
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intenFilter = IntentFilter(Intent.ACTION_BATTERY_LOW)
+        registerReceiver(receiver, intenFilter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 }
+
---
+++ app/src/main/java/ru/com/vbulat/braodcast/MyReceiver.kt
@@ -0,0 +1,18 @@
+package ru.com.vbulat.braodcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyReceiver : BroadcastReceiver() {
+
+    override fun onReceive(p0 : Context?, p1 : Intent?) {
+
+        when (p1?.action) {
+            Intent.ACTION_BATTERY_LOW -> {
+                Toast.makeText(p0, "ACTION_BATTERY_LOW", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
