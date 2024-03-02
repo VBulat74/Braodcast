35e3bcf7e957a71a6116f93a5d5aff663d2019d8
VBulat74 <Vladimir.Bulat@gmail.com>
2024-03-02 17:50:46 +0300

    Lesson 05 (LocalBroadcastManager)

--- app/src/main/java/ru/com/vbulat/braodcast/MainActivity.kt
+++ app/src/main/java/ru/com/vbulat/braodcast/MainActivity.kt
@@ -4,26 +4,29 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var progressBar : ProgressBar
+    private lateinit var progressBar: ProgressBar
+
+    private val localBroadcastManager by lazy { LocalBroadcastManager.getInstance(this) }
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(p0: Context?, p1: Intent?) {
             if (p1?.action == "loaded") {
-                val percent : Int = p1.getIntExtra("percent", 0)
+                val percent: Int = p1.getIntExtra("percent", 0)
                 progressBar.progress = percent
             }
         }
     }
     private var count = 0
 
-    override fun onCreate(savedInstanceState : Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -32,14 +35,14 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button).setOnClickListener {
             Intent(MyReceiver.ACTION_CLICKED).apply {
                 putExtra(MyReceiver.EXTRA_COUNT, ++count)
-                sendBroadcast(this)
+                localBroadcastManager.sendBroadcast(this)
             }
         }
 
         val intenFilter = IntentFilter().apply {
             addAction("loaded")
         }
-        registerReceiver(receiver, intenFilter)
+        localBroadcastManager.registerReceiver(receiver, intenFilter)
 
         Intent(this, MyService::class.java).apply {
             startService(this)
@@ -48,7 +51,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        unregisterReceiver(receiver)
+        localBroadcastManager.unregisterReceiver(receiver)
     }
 }
 
