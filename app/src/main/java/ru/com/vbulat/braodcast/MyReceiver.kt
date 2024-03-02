57a244505036bd06872f70b4b59741716288cc9c
VBulat74 <Vladimir.Bulat@gmail.com>
2024-03-02 15:18:27 +0300

    Lesson 03 (Custom broadcast receiver)

--- app/src/main/java/ru/com/vbulat/braodcast/MyReceiver.kt
+++ app/src/main/java/ru/com/vbulat/braodcast/MyReceiver.kt
@@ -10,6 +10,16 @@ class MyReceiver : BroadcastReceiver() {
     override fun onReceive(p0 : Context?, p1 : Intent?) {
 
         when (p1?.action) {
+
+            ACTION_CLICKED ->{
+                val count: Int = p1.getIntExtra(EXTRA_COUNT, 0)
+                Toast.makeText(
+                    p0,
+                    "ACTION_CLICKED ($count)",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
             Intent.ACTION_BATTERY_LOW -> {
                 Toast.makeText(
                     p0,
@@ -20,8 +30,18 @@ class MyReceiver : BroadcastReceiver() {
 
             Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
                 val turnedOn = p1.getBooleanExtra("state", false)
-                Toast.makeText(p0, "ACTION_AIRPLANE_MODE_CHANGED (${turnedOn.toString()})", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    p0,
+                    "ACTION_AIRPLANE_MODE_CHANGED (${turnedOn.toString()})",
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
+    }
+
+    companion object{
+
+        const val ACTION_CLICKED = "clicked"
+        const val EXTRA_COUNT = "count"
     }
 }
